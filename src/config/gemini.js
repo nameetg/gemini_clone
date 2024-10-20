314b2f6faecfd158d609562c698042a1351d7854
Nameet <nameetsn@gmail.com>
2024-10-21 00:33:03 +0530

    first commit

---
+++ src/config/gemini.js
@@ -0,0 +1,35 @@
+// const myApiKey = "";
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai"
+
+const apiKey = "AIzaSyCAMIRrGXiTBoo5UUR2fzdYrJCe_5y7Fko";
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [
+    ],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  return result.response.text();
+}
+
+export default run;
